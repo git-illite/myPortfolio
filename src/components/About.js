e6bd770f5596a6d02481df4f40bc59d6b478a155
git-illite <71469298+git-illite@users.noreply.github.com>
2023-06-04 04:52:39 -0400

    updated about page

--- src/components/About.js
+++ src/components/About.js
@@ -6,14 +6,14 @@ const About = () => {
           <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
               <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-                Hi, I'm Reed.
+                Hi, I'm Abdallah.
                 <br className="hidden lg:inline-block" />I love to build amazing
                 apps.
               </h1>
               <p className="mb-8 leading-relaxed">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-                laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-                Laborum, voluptas natus?
+              I am recent graduate of Seneca College's Computer Programming and Analysis program. I am a skilled programmer with
+               a strong understanding of software development principles and practices. I am proficient in various programming 
+               languages and experienced in problem-solving and analytical thinking.
               </p>
               <div className="flex justify-center">
                 <a
