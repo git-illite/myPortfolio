7ce05ece0345afc7060d53be31b596508bffddb2
git-illite <71469298+git-illite@users.noreply.github.com>
2023-06-04 04:27:54 -0400

    initial commit

---
+++ src/data.js
@@ -0,0 +1,89 @@
+export const projects = [
+  {
+    title: "React CitiBike Trips",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "https://elaborate-peony-6b2d1c.netlify.app/",
+    sourceCodeLink:"https://github.com/git-illite/reactTrips"
+  },
+  {
+    title: "Spotify API",
+    subtitle: "MEAN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://stellar-pudding-3ef9f8.netlify.app/",
+    sourceCodeLink:"https://github.com/git-illite/SpotifyAngularAAden"
+  },
+  {
+    title: "Muzik Biz",
+    subtitle: "C# ASP.net",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://aaaden1-wa-web524-a6.azurewebsites.net/",
+    sourceCodeLink:"https://github.com/git-illite/MusicBizASPApp"
+  },
+  {
+    title: "Fragments App",
+    subtitle: "Amazon Web Services (AWS)",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://www.youtube.com/watch?v=bEDtSuAfgRQ",
+    sourceCodeLink:"https://github.com/AbdallaAden/fragments"
+  },
+  {
+    title: "C++ App",
+    subtitle: "CPP",
+    description:
+      "The purpose of this project is to put my C++ Object Oriented skills to practice by developing a simulation of an assembly line with any number of stations.",
+    image: "./project-5.gif",
+    link: "https://github.com/git-illite/ms03#readme",
+    sourceCodeLink:"https://github.com/git-illite/ms03"
+  },
+  {
+    title: "HTML & CSS",
+    subtitle: "Dom Manipulation HTML/CSS",
+    description:
+      "The purpose of this project is to put my C++ Object Oriented skills to practice by developing a simulation of an assembly line with any number of stations.",
+    image: "./project-6.gif",
+    link: "https://git-illite.github.io/FinalAssignment/",
+    sourceCodeLink:"https://github.com/git-illite/FinalAssignment"
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Angular",
+  "MongoDB/Mongoose",
+  "SQL",
+  "C++",
+  "C",
+  "C#",
+  "ASP.net",
+  "Java",
+  "Azure",
+  "AWS",
+  "Unreal Blueprints",
+];
